0273ff388b683839c3f4cfb5da1a627ebef2ffeb
Ibi Hasanli <ibili73@gmail.com>
2026-08-04 12:25:23 +0100

    Added LocationControllerIntegrationTest for initial location integration tests

--- src/test/java/uk/co/eightmile/racs/locations/LocationControllerIntegrationTest.java
+++ src/test/java/uk/co/eightmile/racs/locations/LocationControllerIntegrationTest.java
@@ -1,4 +1,55 @@
 package uk.co.eightmile.racs.locations;
 
-public class LocationControllerIntegrationTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import uk.co.eightmile.racs.auth.AuthContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class LocationControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @MockitoBean
+    private AuthContext authContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
+        Mockito.when(authContext.getUserId()).thenReturn(null);
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    @WithMockUser
+    void getLocations_returns200() throws Exception {
+        mockMvc.perform(get("/api/location"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }
